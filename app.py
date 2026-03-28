9a3626d29c104009f7eb2acff5a3f098903f8025
Leah Mogotsi <202100755@ub.ac.bw>
2026-03-28 04:37:25 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+ 
+
+import streamlit as st
+import joblib
+import numpy as np
+
+model = joblib.load("nltk.pkl")
+
+st.title("Spam Detection App")
+
+text = st.number_input("Enter message received")
+
+
+if st.button("Predict"):
+
+    features = np.array([[text]])
+
+    prediction = model.predict(features)
+
+    st.success(f"Estimated Price: {prediction[0]}")
+    
